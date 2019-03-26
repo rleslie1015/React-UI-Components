12bd81df5ea69d4af202a7659235f09972b56150
lrod <rleslie1015@gmail.com>
2019-03-25 18:48:43 -0700

    took out component from header container

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from "./ImageThumbnail";
 import HeaderTitle from "./HeaderTitle";
-import HeaderContent from "./HeaderContent";
+
 
 function HeaderContainer () {
     return <div className ="headerClass">
