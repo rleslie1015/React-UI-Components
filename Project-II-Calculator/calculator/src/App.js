92ac56e4e0ccef445e6e74fa5809f23c5e5a789e
lrod <rleslie1015@gmail.com>
2019-03-26 19:21:04 -0700

    all the number buttons are layed out

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import './App.css';
+import NumberButton from "./components/ButtonComponents/NumberButton";
 
 const App = () => {
   return (
+    
     <div>
+      <div>
+    <NumberButton text="7"/>
+      <NumberButton text="8"/>
+      <NumberButton text="9"/>
+      </div>
+      <div>
+    <NumberButton text="4"/>
+      <NumberButton text="5"/>
+      <NumberButton text="6"/>
+      </div>
+      <div>
+
+<NumberButton text="1"/>
+      <NumberButton text="2"/>
+      <NumberButton text="3"/>
+      </div>
+
+  
+      
       <h3>Welcome to React Calculator</h3>
       <p>
         We have given you a starter project. You'll want to build out your
